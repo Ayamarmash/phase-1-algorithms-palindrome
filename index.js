d89869a926804f232a1d032e2b9ffedc68dba261
root <root@DESKTOP-JFEPESN.localdomain>
2022-11-06 17:41:12 +0200

    Done

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ function isPalindrome(word) {
   if (wordBackward == word) return true
   return false
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
